bf34e13a0e63ca58112c0e374f423c5d8512630d
Mitta Sai Nitish <82138518+sainitishmitta04@users.noreply.github.com>
2024-02-19 12:03:23 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-# app.py (combined)
-from flask import Flask, render_template, request
-from flask_cors import CORS
 import streamlit as st
 import pickle
 import time
@@ -8,66 +5,70 @@ from langchain import OpenAI
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredURLLoader
+# from langchain_community.document_loaders import SeleniumURLLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
-import os
-from dotenv import load_dotenv
+# from langchain_community.document_loaders import WebBaseLoader
 
+import os
+
+from dotenv import load_dotenv
 load_dotenv()
 
-app = Flask(__name__)
-CORS(app)
-
 urls = [
-    "https://marg-darshan.com/",  # margdarshan website
-    "https://jeemain.nta.nic.in/about-jeemain-2023/",  # Govt NTA
     "https://byjus.com/jee/jee-main/",
     "https://engineering.careers360.com/articles/jee-main-2024"
 ]
 
+# process_url_clicked = st.sidebar.button("Process URLs")
 file_path = "faiss_store_openai.pkl"
 
-llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0.7, max_tokens=500)
-
+main_placeholder = st.empty()
+# llm = OpenAI(temperature=0.8, max_tokens=500)
+llm = OpenAI(model="gpt-3.5-turbo-instruct",temperature=0.7,max_tokens=500)
+# if process_url_clicked:
+# load data
 loader = UnstructuredURLLoader(urls=urls)
+# loader = SeleniumURLLoader(urls=urls)
+# loader = WebBaseLoader(urls)
+main_placeholder.text("Data Loading...Started...✅✅✅")
 data = loader.load()
-
+# split data
 text_splitter = RecursiveCharacterTextSplitter(
     separators=['\n\n', '\n', '.', ','],
     chunk_size=1000
 )
+main_placeholder.text("Text Splitter...Started...✅✅✅")
 docs = text_splitter.split_documents(data)
-
+# create embeddings and save it to FAISS index
 embeddings = OpenAIEmbeddings()
 vectorstore_openai = FAISS.from_documents(docs, embeddings)
+main_placeholder.text("Embedding Vector Started Building...✅✅✅")
+time.sleep(2)
 
+# Save the FAISS index to a pickle file
 with open(file_path, "wb") as f:
     pickle.dump(vectorstore_openai, f)
 
-
-@app.route('/')
-def index():
-    return render_template('./home.html')
-
-
-@app.route('/get_response', methods=['POST'])
-def get_response():
-    user_input = request.get_json()
-    user_query = user_input["user_input"]
-
+query = main_placeholder.text_input("Question: ")
+if query:
     if os.path.exists(file_path):
         with open(file_path, "rb") as f:
             vectorstore = pickle.load(f)
             chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
-            result = chain({"question": user_query}, return_only_outputs=True)
-            response = {
-                "answer": result.get("answer", ""),
-                "sources": result.get("sources", "")
-            }
-            return response
-    else:
-        return {"error": "FAISS index not found"}
+            result = chain({"question": query}, return_only_outputs=True)
+            # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+            st.header("Answer")
+            st.write(result["answer"])
+
+            # Display sources, if available
+            sources = result.get("sources", "")
+            if sources:
+                st.subheader("Sources:")
+                sources_list = sources.split("\n")  # Split the sources by newline
+                for source in sources_list:
+                    st.write(source)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+
